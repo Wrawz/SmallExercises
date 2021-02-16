9e540fa9d7ed2113cf6aaf9743b640a912c0a314
Wrawz <68308481+Wrawz@users.noreply.github.com>
2021-02-16 13:30:19 -0300

    Update database.py

--- practicing_some_python_db/utils/database.py
+++ practicing_some_python_db/utils/database.py
@@ -125,20 +125,14 @@ def query_book_id_by_its_full_title(book_title: str) -> int:
 # SELECT * FROM book;
 
 
-def get_all_authors():
-    """
-    returns a list in json format
-    """
+def get_all_authors() -> List[Book]:
     with MySQLDbConnection as db:
         with db.cursor() as cursor:
             cursor.execute("SELECT name, birth, country FROM author")
             return [{'name': row[0], 'birth': row[1], 'country': row[2]} for row in cursor.fetchall()]
 
 
-def get_all_books():
-    """
-    returns a list in json format
-    """
+def get_all_books() -> List[Book]:
     with MySQLDbConnection() as db:
         with db.cursor() as cursor:
             cursor.execute("SELECT title, yearReleased, isRead FROM book")
